0dfabc11982c38c1b004a9d6a50be7ff1172d370
Sai Kasam <saikasam98@gmail.com>
2020-05-19 13:55:12 -0500

    able to log into liquidation through user input

--- backend/seed_data.py
+++ backend/seed_data.py
@@ -14,5 +14,43 @@ def main():
     manifest = {"Name":"Manifest1 Test"}
     manifests_id = manifests_collection.insert_one(manifest).inserted_id
 
+    browser = logIn()
 
+
+def logIn():
+    print("Trying to log in")
+    # Connect to Google
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open("https://www.liquidation.com/login")
+    
+    browser.get_current_page()
+    # Fill-in the form
+    browser.select_form('form[id="loginForm"]')
+
+    # browser.get_current_form().print_summary()
+    browser["j_username"] = input("User Name: ")
+    browser["j_password"] = input("Password: ")
+
+    browser.submit_selected()
+    stall(3)
+    #log in check
+    browser.open("https://www.liquidation.com/account/main")
+    soup = browser.get_current_page()
+    try:
+        name = soup.find(id='signDetails').span.get_text()
+        if name == None:
+            return False
+        print(name)
+        print("Logged In")
+        return True
+    except:
+        print("Not Logged In")
+        return False
+
+    return(browser)
+def stall(sec):
+    start = time.time()
+    end = time.time()
+    while end - start < sec:
+        end = time.time()
 main()
