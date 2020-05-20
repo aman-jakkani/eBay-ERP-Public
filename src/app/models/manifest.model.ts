4527fac48983695a80f8b91bc8fdf6c53cd7c66b
Sai Kasam <saikasam98@gmail.com>
2020-05-20 16:31:45 -0500

    created front end models

---
+++ src/app/models/manifest.model.ts
@@ -0,0 +1,11 @@
+export interface Manifest {
+    id: string;
+    auction_title: string;
+    auction_id: number;
+    transaction_id: number;
+    quantity: number;
+    total_price: number;
+    date_purchased: Date;
+    status: string;
+  }
+  
---
+++ src/app/models/product.model.ts
@@ -0,0 +1,10 @@
+export interface Manifest {
+    id: string;
+    title: string;
+    quantity: number;
+    price: number;
+    model: string;
+    grade: string;
+    manifest_id: string;
+  }
+  
