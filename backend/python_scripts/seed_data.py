16645c5cc3238679ed2bb33d604636102fb77594
Sai Kasam <saikasam98@gmail.com>
2020-05-23 16:55:57 -0500

    added comments

--- backend/python_scripts/seed_data.py
+++ backend/python_scripts/seed_data.py
@@ -124,6 +124,7 @@ def saveItems(browser, manifests):
             product_headers = ["sku","quantity_sold","prices_sold"]
             product_count = products_collection.count_documents({})
             product_count = "{0:0=6}".format(product_count)
+            #Creating Unique SKU
             sku = item['name'].split()[1] + str(product_count)
             product = {
                 "sku": sku,
