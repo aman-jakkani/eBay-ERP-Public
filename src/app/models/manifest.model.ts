a30782681f8890fbb577543dd90ac793d060429d
Sai Kasam <saikasam98@gmail.com>
2020-05-26 18:11:56 -0500

    trying manifest model class

--- src/app/models/manifest.model.ts
+++ src/app/models/manifest.model.ts
@@ -1,11 +1,43 @@
-export interface Manifest {
-    id: string;
-    auction_title: string;
-    auction_id: number;
-    transaction_id: number;
-    quantity: number;
-    total_price: number;
-    date_purchased: Date;
-    status: string;
-  }
+
+
+export class Manifest  {
+   id: string;
+   auction_title: string;
+   auction_id: number;
+   transaction_id: number;
+   quantity: number;
+   total_price: number;
+   date_purchased: Date;
+   status: string;
   
+  constructor({
+    id,
+    auction_title,
+    auction_id,
+    transaction_id,
+    quantity,
+    total_price,
+    date_purchased,
+    status}:{
+    id: string,
+    auction_title: string,
+    auction_id: number,
+    transaction_id: number,
+    quantity: number,
+    total_price: number,
+    date_purchased: Date,
+    status: string
+  }){
+
+      this.id = id;     
+      this.auction_title = auction_title;
+      this.auction_id = auction_id;
+      this.transaction_id = transaction_id;
+      this.quantity = quantity;
+      this.total_price = total_price;
+      this.date_purchased = date_purchased;
+      this.status = status;
+  }
+
+
+}
