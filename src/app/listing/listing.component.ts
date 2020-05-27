8524b6789d1fa86a7dea3721930a62cddbd09b75
TheKasam <saikasam98@gmail.com>
2020-05-27 12:41:35 -0500

    added formatting and updated draft instance instead of getDrafts. Also geting indec to get item for updateSKU instead of product_id

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -3,12 +3,12 @@ import { MainService } from '../main.service';
 import {Router} from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { observable, VirtualTimeScheduler } from 'rxjs';
-import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import {Manifest} from '../models/manifest.model';
 import { Product } from '../models/product.model';
 import { Item } from '../models/item.model';
 import { Draft } from '../models/draft.model';
-import { formatDate } from "@angular/common";
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -18,17 +18,17 @@ import { formatDate } from "@angular/common";
 export class ListingComponent implements OnInit {
 
 
-  //total quantity of items in auction
-  manifestQuantity: number = 0;
-  //total sum of prices estimated by auction
-  priceTotal: number = 0;
-  //Contain list of all Manifests
+  // total quantity of items in auction
+  manifestQuantity = 0;
+  // total sum of prices estimated by auction
+  priceTotal = 0;
+  // Contain list of all Manifests
   manifests: Manifest[];
-  //Contain items of current manifest
+  // Contain items of current manifest
   items: Item[];
-  //Contains current manifest
+  // Contains current manifest
   current_manifest: Manifest;
-  //products of current manifest
+  // products of current manifest
   products: Product[] = [];
   draft: FormGroup;
   ttInput: string;
@@ -38,67 +38,67 @@ export class ListingComponent implements OnInit {
 
   ngOnInit() {
     this.getManifests();
-    //Form for link input
+    // Form for link input
     this.draft = new FormGroup({
       sku: new FormControl(null, {}),
       title: new FormControl(null, {}),
-      condition: new FormControl("Used", {}),
+      condition: new FormControl('Used', {}),
       conditionDesc: new FormControl(null, {}),
       price: new FormControl(null, {}),
     });
 
   }
 
-  getManifests(){
+  getManifests() {
     this.mainService.getManifests().subscribe(
       data => {
         this.manifests = data;
-        this.manifests.sort((a,b) => (a.date_purchased < b.date_purchased)?1 : -1);
+        this.manifests.sort((a, b) => (a.date_purchased < b.date_purchased) ? 1 : -1);
         this.manifests.forEach(element => {
-          formatDate(element.date_purchased,'mm/DD/yyyy', 'en-US');
+          formatDate(element.date_purchased, 'mm/DD/yyyy', 'en-US');
         });
       });
   }
 
-  getManifestDetails(manifestID){
-    //reseting vars
+  getManifestDetails(manifestID) {
+    // reseting vars
     this.priceTotal = 0;
     this.getManifest(manifestID);
     this.getItems(manifestID);
   }
 
-  getManifest(manifestID){
+  getManifest(manifestID) {
     this.mainService.getManifest(manifestID).subscribe(
       data => {
         console.log((data));
         this.current_manifest = data;
-        //Getting Quantity
+        // Getting Quantity
         this.manifestQuantity = this.current_manifest.quantity;
       });
   }
 
-  getItems(manifestID){
+  getItems(manifestID) {
     this.mainService.getItems(manifestID).subscribe(
       data => {
         this.items = data;
-        //Getting Items Total Value
-        for ( var item of data){
+        // Getting Items Total Value
+        for ( const item of data) {
           this.priceTotal += item.price * item.quantity;
         }
-        //clearing previous products
+        // clearing previous products
         this.getProducts();
         // console.log("Logging products",this.products);
         this.getDrafts();
-        console.log("got drafts", this.drafts);
+        console.log('got drafts', this.drafts);
       });
   }
-  getDrafts(){
+  getDrafts() {
     this.drafts = [];
 
     // anonymous async function to get drafts
     (async () => {
 
-      for (var item of this.items) {
+      for (const item of this.items) {
 
         const item_id = item.id;
 
@@ -106,22 +106,22 @@ export class ListingComponent implements OnInit {
           this.mainService.getDraft(item_id).subscribe(
             data => {
               this.drafts.push(data);
-              resolve("Got Product!!");
+              resolve('Got Product!!');
           });
         });
 
         await one;
       }
-      return "out of loop"
+      return 'out of loop';
     })();
   }
-  getProducts(){
+  getProducts() {
     this.products = [];
 
     // anonymous async function to get products
     (async () => {
 
-      for (var item of this.items) {
+      for (const item of this.items) {
 
         const item_id = item.id;
 
@@ -129,36 +129,36 @@ export class ListingComponent implements OnInit {
           this.mainService.getProduct(item_id).subscribe(
             data => {
               this.products.push(data);
-              resolve("Got Product!!");
+              resolve('Got Product!!');
           });
         });
 
         await one;
       }
-      return "out of loop"
+      return 'out of loop';
     })();
 
 
 
   }
 
-  updateSKU(productID, newSKU, i){
-    var itm = this.items.filter(x => x.product_id == productID);
-    this.mainService.updateSKU(itm[0].id, newSKU).subscribe(data => {
-      var product:Product = data;
+  updateSKU(productID, newSKU, i) {
+    // const itm = this.items.filter(x => x.product_id === productID);
+    this.mainService.updateSKU(this.items[i].id, newSKU).subscribe(data => {
+      const product: Product = data;
       console.log(data);
       this.products[i] = product;
-      this.getDrafts();
+      this.drafts[i].updated_SKU = true;
     });
   }
-  updateSKUAgain(productID, newSKU, i){
-    alert("Updating your SKU may affect your tracking capabilities");
-    var itm = this.items.filter(x => x.product_id == productID);
-    this.mainService.updateSKU(itm[0].id, newSKU).subscribe(data => {
-      var product:Product = data;
+  updateSKUAgain(productID, newSKU, i) {
+    alert('Updating your SKU may affect your tracking capabilities');
+    // const itm = this.items.filter(x => x.product_id === productID);
+    this.mainService.updateSKU(this.items[i], newSKU).subscribe(data => {
+      const product: Product = data;
       console.log(data);
       this.products[i] = product;
-      this.getDrafts();
+      this.drafts[i].updated_SKU = true;
 
     });
   }
