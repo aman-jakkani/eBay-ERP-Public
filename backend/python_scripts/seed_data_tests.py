2f96b2d05537a0709c583da440078af2e9d82cd3
TheKasam <saikasam98@gmail.com>
2020-05-27 22:19:02 -0500

    update still now workign

--- backend/python_scripts/seed_data_tests.py
+++ backend/python_scripts/seed_data_tests.py
@@ -3,24 +3,56 @@ import mechanicalsoup
 import sys
 import time
 from pymongo import MongoClient
-import getpass 
+import getpass
 from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+import requests
+from requests_html import HTMLSession
 
 
 def main():
 
-    logIn()
+    session = HTMLSession()
+    r = session.get("https://techliquidators.com/index.cfm/p/7")
+    # r.html.render()
+
+    browser = logIn()
+    manifests_list = saveManifests(browser)
+
+
+def saveManifests(browser):
+
+
+    browser.open("https://techliquidators.com/index.cfm/p/7")
+    soup = browser.get_current_page()
+
+
+
+    transactions_in_progress = soup.prettify() #.find("div",{"id":{"no-more-tables"}}).table
+
+    print(transactions_in_progress)
+
+    #mongo attributes for manifest collection
+    headers = ["auction_title", "auction_id", "transaction_id","quantity","total_price","date_purchased","status","source"]
+
+    browser.download_link("https://techliquidators.com/csincludes/_wonAuctions_Query2Excel.cfm?UserID=1987480")
+
+
+    return []
 
 #tech liquidation log in test
 def logIn():
 
     loggedIn = False
     while loggedIn == False:
-        print("Trying to log in to liquidation")
-        # Connect to Google
+        print("Trying to log in to Tech Liquidators")
+
+
         browser = mechanicalsoup.StatefulBrowser()
+        browser.session.headers.update({ 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
+
         browser.open("https://techliquidators.com/tl/index.cfm?action=account_login")
-        
+
         browser.get_current_page()
         # Fill-in the form
         browser.select_form('form[id="login"]')
@@ -37,20 +69,20 @@ def logIn():
             name = soup.find("div",{"class" : "col-md-8 text-right"}).small.get_text()
             if name == None:
                 print("Not logged in. Try again.")
-                
-           
+
+
             elif "Welcome" in name:
                 print(name)
                 print("Logged In")
                 loggedIn = True
             else:
                 print("Not logged in. Try again.")
-        
+
         except:
 
             print("Not logged in. Try again.")
 
-       
+
 
     return(browser)
 def stall(sec):
