007f3a3c181433fab87603c1ea70b0ae97fb542a
Aman Jakkani <aj280598@gmail.com>
2020-05-30 13:42:08 -0500

    header changes based on auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,17 +7,24 @@ import { stringify } from '@angular/compiler/src/util';
 import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 import { AuthData } from "./auth-data.model";
+import { Subject } from 'rxjs';
 
 const BACKEND_URL = environment.apiUrl ;
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private token: string;
+  private authStatusListener = new Subject<boolean>();
   constructor(private http: HttpClient) {}
 
   getToken(){
     return this.token;
   }
+
+  getAuthStatusListener(){
+    return this.authStatusListener.asObservable();
+  }
+
   createUser(email: string, password: string){
     const authData: AuthData = {email: email, password: password};
     this.http.post(BACKEND_URL+'/users/signup', authData).subscribe(response => {
@@ -30,7 +37,7 @@ export class AuthService {
     this.http.post<{token: string}>(BACKEND_URL+'/users/login', authData).subscribe(response => {
       const token = response.token;
       this.token = token;
-
+      this.authStatusListener.next(true);
     });
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { MainService } from '../main.service';
 import {Router} from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
+export class HeaderComponent implements OnInit, OnDestroy {
+userIsAuth = false;
 form: FormGroup;
+private authListenerSubs: Subscription;
 
-
-constructor(public mainService: MainService, private router: Router) {}
+constructor(public mainService: MainService, private router: Router, private authService: AuthService) {}
 
 ngOnInit(){
   this.form = new FormGroup({
     search: new FormControl(null, {}),
   });
+  this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuth => {
+    this.userIsAuth = isAuth;
+  });
+}
+
+ngOnDestroy(){
+  this.authListenerSubs.unsubscribe();
 }
 onSearch(){
   var searchValue = this.form.value.search
