741b33708269d22c775f0d84a75b39c36f28b7e4
Sai Kasam <saikasam98@gmail.com>
2020-06-02 16:14:26 -0500

    progress on running script in node, able to link to python script and run

--- backend/routes/listing.js
+++ backend/routes/listing.js
@@ -10,6 +10,10 @@ const Item = require("../models/item");
 const Draft = require("../models/draft");
 const checkAuth = require("../middleware/check-auth");
 
+const prompt = require('prompt-sync')();
+const {spawn} = require('child_process');
+
+
 //getting all manifests from database
 router.get("/getLiquidationManifests", checkAuth, (req, res) => {
 
@@ -28,17 +32,54 @@ router.get("/getLiquidationManifests", checkAuth, (req, res) => {
 
 router.get("/getTechManifests", checkAuth, (req, res) => {
 
-  var url = req.params.url;
-  console.log(req.params);
+  // var url = req.params.url;
+  // console.log(req.params);
 
-  Manifest.find({source: "techliquidators.com"}).then(documents => {
-    console.log(documents);
-    res.status(200).json({
-      message: "Tech Liquidation Manifests fetched succesfully",
-      manifests: documents
-    });
+  // Manifest.find({source: "techliquidators.com"}).then(documents => {
+  //   console.log(documents);
+  //   res.status(200).json({
+  //     message: "Tech Liquidation Manifests fetched succesfully",
+  //     manifests: documents
+  //   });
+  // });
+
+  //USING FOR SEEDDATA TESTING
+  const username = prompt('What is your username?');
+  const password = prompt('What is your password?');
+
+  	// spawn new child process to call the python script
+  const python = spawn('python3', ['../backend/python_scripts/seed_data_liquidation.py',username,password]);
+	// collect data from script
+	python.stdout.on('data', function (data) {
+
+    pythonData = data;
+    console.log(uint8arrayToString(data));
   });
 
+
+	// in close event we are sure that stream is from child process is closed
+	python.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+  
+    // res.status(200).json({
+    //   message: "got link data",
+    //   data: JSON.parse(largeDataSet.join(""))
+    // });
+  });
+
+  var uint8arrayToString = function(data){
+    return String.fromCharCode.apply(null, data);
+  };
+
+  python.stderr.on('data', (data) => {
+    // As said before, convert the Uint8Array to a readable string.
+    console.log("stderr");
+    console.log(uint8arrayToString(data));
+  });
+  
+  python.on('exit', (code) => {
+    console.log("Process quit with code : " + code);
+  });
 });
 
 //getting a specific manifest
@@ -242,11 +283,13 @@ router.get("/getLinkData/:url/:siteNum", (req, res) => {
   var uint8arrayToString = function(data){
     return String.fromCharCode.apply(null, data);
   };
+
   python.stderr.on('data', (data) => {
     // As said before, convert the Uint8Array to a readable string.
     console.log("stderr");
     console.log(uint8arrayToString(data));
   });
+  
   python.on('exit', (code) => {
     console.log("Process quit with code : " + code);
   });
