16808a71a775d70dccfb481b9692b234147b3af1
Sai Kasam <saikasam98@gmail.com>
2020-06-24 10:46:06 -0500

    fixed unresponsive server error

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,6 @@
 const app = require("./app");
 const debug = require("debug")("node-angular");
 const http = require("http");
-var path = require('path')
-var fs = require('fs')
-var https = require('https')
-
-// var certOptions = {
-//   key: fs.readFileSync('../../local-cert-generator/server.key'),
-//   cert: fs.readFileSync('../../local-cert-generator/server.crt')
-// }
 
 const normalizePort = val => {
   var port = parseInt(val, 10);
@@ -54,9 +46,69 @@ const onListening = () => {
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
-// const server = https.createServer(certOptions, app)
-const server = https.createServer(app);
-
+const server = http.createServer(app);
 server.on("error", onError);
 server.on("listening", onListening);
 server.listen(port);
+// const app = require("./app");
+// const debug = require("debug")("node-angular");
+// const http = require("http");
+// var path = require('path')
+// var fs = require('fs')
+// var https = require('https')
+
+// // var certOptions = {
+// //   key: fs.readFileSync('../../local-cert-generator/server.key'),
+// //   cert: fs.readFileSync('../../local-cert-generator/server.crt')
+// // }
+
+// const normalizePort = val => {
+//   var port = parseInt(val, 10);
+
+//   if (isNaN(port)) {
+//     // named pipe
+//     return val;
+//   }
+
+//   if (port >= 0) {
+//     // port number
+//     return port;
+//   }
+
+//   return false;
+// };
+
+// const onError = error => {
+//   if (error.syscall !== "listen") {
+//     throw error;
+//   }
+//   const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+//   switch (error.code) {
+//     case "EACCES":
+//       console.error(bind + " requires elevated privileges");
+//       process.exit(1);
+//       break;
+//     case "EADDRINUSE":
+//       console.error(bind + " is already in use");
+//       process.exit(1);
+//       break;
+//     default:
+//       throw error;
+//   }
+// };
+
+// const onListening = () => {
+//   const addr = server.address();
+//   const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+//   debug("Listening on " + bind);
+// };
+
+// const port = normalizePort(process.env.PORT || "3000");
+// app.set("port", port);
+
+// // const server = https.createServer(certOptions, app)
+// const server = https.createServer(app);
+
+// server.on("error", onError);
+// server.on("listening", onListening);
+// server.listen(port);
