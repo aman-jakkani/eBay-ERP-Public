26d42373ab97e24359ea7872fafd550c75b28b1b
Sai Kasam <saikasam98@gmail.com>
2020-06-27 17:16:19 -0500

    meging back

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ const app = require("./app");
 const debug = require("debug")("node-angular");
 const http = require("http");
 
-// var https = require("https");
-// var fs = require("fs");
-// var path = require("path");
+//comment out prod
+var https = require("https");
+var fs = require("fs");
+var path = require("path");
 
 const normalizePort = val => {
   var port = parseInt(val, 10);
@@ -47,15 +48,16 @@ const onListening = () => {
   debug("Listening on " + bind);
 };
 
-// var certOptions = {
-//   key: fs.readFileSync(path.resolve('./local-cert-generator/server.key')),
-//   cert: fs.readFileSync(path.resolve('./local-cert-generator/server.crt'))
-// }
+//comment out prod
+var certOptions = {
+  key: fs.readFileSync(path.resolve('../../local-cert-generator/server.key')),
+  cert: fs.readFileSync(path.resolve('../../local-cert-generator/server.crt'))
+}
 const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
-// var server = https.createServer(certOptions, app);
-const server = http.createServer(app);
+var server = https.createServer(certOptions, app);
+// const server = http.createServer(app);
 
 server.on("error", onError);
 server.on("listening", onListening);
